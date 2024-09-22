db1b0eed61d7a89225984c5c530e8d1e0273e26b
Cristina Semikina <cristinasemikina@gmail.com>
2024-09-22 10:55:23 +0200

    Project Initialization

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.habittracker {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.habittracker to javafx.fxml;
+    exports com.example.habittracker;
+}
