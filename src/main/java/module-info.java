8b92d537994633818dd9a950f1046331535ee04c
Aleksandra Bruska <s185454@student.pg.edu.pl>
2024-10-24 11:01:24 +0200

    Sending welcome e-mails

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.habittracker {
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires jakarta.mail;
 
     opens com.example.habittracker to javafx.fxml;
     exports com.example.habittracker;
