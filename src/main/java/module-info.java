4d1aaef7197cbc6d4a668305e3edb5a6b4622c7b
Cristina Semikina <cristinasemikina@gmail.com>
2024-11-12 10:56:47 +0100

    Google Calendar Setup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,15 @@ module com.example.habittracker {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
 
+    requires google.api.client;
+    requires com.google.api.client.auth;
+    requires com.google.api.client;
+    requires com.google.api.client.json.gson;
+    requires google.api.services.calendar.v3.rev411;
+    requires com.google.api.client.extensions.jetty.auth;
+    requires com.google.api.client.extensions.java6.auth;
+    requires jdk.httpserver;
+
     opens com.example.habittracker to javafx.fxml;
     exports com.example.habittracker;
 }
